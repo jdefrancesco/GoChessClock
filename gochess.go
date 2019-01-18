90fb77d85f325f31f1516b448b063c64db2fab7f
jdefr89 <jdefr89@gmail.com>
2019-01-18 13:02:41 -0500

    Added core chess clock structures/logic
    Added type ChessClock, a structure containing the state of
    the chess clock, and game states related to time.
    
    Main function has some core logic in it for changes
    that alter the time.

--- gochess.go
+++ gochess.go
@@ -1,26 +1,54 @@
 package main
 
 import (
-    "fmt"
-    "time"
-    "github.com/jroimartin/gocui"
+	"fmt"
+	"log"
+	"time"
+
+	_ "github.com/jroimartin/gocui"
 )
 
+const (
+	// Players, identified by color.
+	WHITE = iota
+	BLACK
 
+	// Game states.
+	GAMEPAUSED
+	GAMERESET
+	GAMEOVER
+)
 
-func main() {
-    log.Println("Started chess clock...")
+type ClockTime uint64
+type GameState uint64
 
-    // Init gocui 
-    g, err := gocui.NewGui(gocui.OutputNormal)
-    if err != nil {
-	log.Panicln(err)
-    }
-    defer g.Close()
+// ChessClock is a structue containing elements
+type ChessClock struct {
+	WhiteTime ClockTime
+	BlackTime ClockTime
 
-    active := make(chan bool)
-    // Two go funcs each with a select checking active flag
-
-
+	// Specify which clock is currently active (ticking down)
+	ActiveClock uint64
+	// State of game (over, paused, active, etc)
+	State uint64
 }
 
+func main() {
+
+	log.Println("Started chess clock main...")
+
+	userInput := make(chan string)
+
+	// Grab user keyboard input
+	go func() { userInput <- os.Stdin.Read(make([]byte, 1)) }()
+
+	for {
+		select {
+		case in := <-userInput:
+			log.Println("Received user input: %c", in)
+		default:
+			log.Println("...")
+			time.Sleep(1 * time.Second)
+		}
+	}
+}
